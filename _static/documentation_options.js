3572f3066c138bd2993d229ef6dc37a71e309503
allen91wu <allen91.wu@gmail.com>
2022-09-23 04:18:13 +0000

    Deploying to gh-pages from  @ 9006f6ec1943f3bbd13965eb29df36f2a2413651 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.10.2',
-    LANGUAGE: 'zh_TW',
+    VERSION: '3.10.7',
+    LANGUAGE: 'zh-TW',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
